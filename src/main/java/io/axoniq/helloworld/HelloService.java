ab7aa80540185b68e463d25ab9c1351957cc515b
Bert Laverman <bertlaverman@MacBook-Pro-4.local>
2018-06-06 12:26:09 +0200

    Small Spring Boot test project

---
+++ src/main/java/io/axoniq/helloworld/HelloService.java
@@ -0,0 +1,13 @@
+package io.axoniq.helloworld;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloService {
+
+    @GetMapping("/")
+    public String hello() {
+        return "Hello world!";
+    }
+}
